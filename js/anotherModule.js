4f3639fd1cefc68c032ee87b859d23e54146dbf5
Francisco Gutierrez <francisco.gutierrez@gig.com>
2020-04-24 16:03:09 +0200

    demo commit

---
+++ js/anotherModule.js
@@ -0,0 +1,1 @@
+export const anotherModule = () => console.log('This is from another module');
---
+++ js/index.js
@@ -0,0 +1,10 @@
+import { anotherModule } from './anotherModule.js'
+
+anotherModule()
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue no compilation!'
+  }
+})
